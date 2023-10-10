8173f9018f156ba7c1d28ff9657dbb34d70ac0fd
YunzheLi <liyunzhe903@shu.edu.cn>
2023-10-10 14:27:05 +0800

    tidy up

--- front/src/components/DetailPannel.vue
+++ front/src/components/DetailPannel.vue
@@ -1,11 +1,11 @@
 <template>
-  <el-card class="box-card detail-panel" shadow="hover">
-    <div slot="header" class="clearfix">
-      <span>节点信息</span>
-    </div>
-    <div class="text-item">
-    </div>
-  </el-card>
+<!--  <el-card class="box-card detail-panel" shadow="hover">-->
+<!--    <div slot="header" class="clearfix">-->
+<!--      <span>节点信息</span>-->
+<!--    </div>-->
+<!--    <div class="text-item">-->
+<!--    </div>-->
+<!--  </el-card>-->
 </template>
 
 <script>
